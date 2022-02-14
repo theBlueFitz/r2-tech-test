5e69897c2dea713fbcf8a1f50d909ea4aaa3b47d
theBlueFitz <seanyh92@gmail.com>
2022-02-14 19:08:40 +0000

    changes server file to more familiar format, add catch all for bad url path and test

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-const server = require('express')();
 const apiRouter = require('./routes/api');
+const express = require('express')
+
+const server = express();
+
+server.use(express.json())
 
 server.use('/api', apiRouter);
+
+server.all("/*", (req, res) => {
+    res.status(400).send({msg : 'Path not found'})
+});
+
 
 module.exports = server;
--- test/api.test.js
+++ test/api.test.js
@@ -1,3 +1,4 @@
+const { iteratee } = require('lodash');
 const supertest = require('supertest');
 const server = require('../server');
 
@@ -7,3 +8,11 @@ test('/api', async () => {
   const { body } = await request.get('/api').expect(200);
   expect(body.message).toBe('ok');
 });
+
+describe('GET /api/somethingunexpected', () => {
+  it('400: responds with an error message for bad urlpath', async () => {
+    const {body} = await request.get('/api/adsf').expect(400);
+    expect(body.msg).toBe('Path not found')
+  })
+})
+
